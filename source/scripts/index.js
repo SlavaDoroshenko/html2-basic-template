4fc4b428b3070495d6afda74f9e2bbf3246232e1
Вячеслав Дорошенко <doroshenko.v.p@edu.mirea.ru>
2024-09-25 10:23:00 +0300

    first commit

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,21 @@
-const toggleButton = document.querySelector('.page-header__toggle');
-const siteMenu = document.querySelector('.site-menu');
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
 
-toggleButton.addEventListener('click', () => {
-  siteMenu.classList.toggle('site-menu--visible');
+const toggleButton = document.querySelector(".page-header__toggle");
+const siteMenu = document.querySelector(".site-menu");
+
+toggleButton.addEventListener("click", () => {
+  siteMenu.classList.toggle("site-menu--visible");
+  toggleButton.classList.toggle("page-header__toggle--open");
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
