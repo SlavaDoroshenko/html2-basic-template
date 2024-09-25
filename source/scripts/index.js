78acb180ff746d0c2ded8ecb43c4ead0af5a0ca4
Вячеслав Дорошенко <doroshenko.v.p@edu.mirea.ru>
2024-09-25 15:29:08 +0300

    ggg

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,20 @@
-import Swiper from "swiper/bundle";
-import "swiper/css/bundle";
+const toggleButton = document.querySelector('.page-header__toggle');
+const siteMenu = document.querySelector('.site-menu');
 
-const toggleButton = document.querySelector(".page-header__toggle");
-const siteMenu = document.querySelector(".site-menu");
-
-toggleButton.addEventListener("click", () => {
-  siteMenu.classList.toggle("site-menu--visible");
-  toggleButton.classList.toggle("page-header__toggle--open");
+toggleButton.addEventListener('click', () => {
+  siteMenu.classList.toggle('site-menu--visible');
+  toggleButton.classList.toggle('page-header__toggle--open');
 });
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
