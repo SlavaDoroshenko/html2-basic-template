460098b9a31bf24e18dfcd863260c5ffd869bcf7
Вячеслав Дорошенко <doroshenko.v.p@edu.mirea.ru>
2024-09-26 11:50:14 +0300

    fisrt

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -18,3 +18,28 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const logoIcon = L.icon({
+    iconUrl: 'images/marker@2x.png', // Ссылка на изображение логотипа
+    iconSize: [38, 50], // Размер иконки
+    iconAnchor: [25, 25], // Координаты привязки иконки
+  });
+
+  // Инициализация карты
+  const map = L.map('map', {
+    zoomControl: false, // Отключение кнопок + и -
+    attributionControl: false, // Отключение надписи справа внизу
+  }).setView([55.681052, 37.526124], 12); // Координаты Москвы
+
+  // Добавление слоя карты
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Добавление маркера
+  const marker = L.marker([55.681052, 37.526124], { icon: logoIcon }).addTo(
+    map
+  );
+});
